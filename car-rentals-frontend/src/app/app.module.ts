09e08b6bb52110504ccca80a30676fce4a2050b7
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-10-15 04:45:19 -0400

    Added Header Component

--- car-rentals-frontend/src/app/app.module.ts
+++ car-rentals-frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
