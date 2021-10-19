af07fe31eb9ce869a476c8666c4bb87a605da42c
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-10-19 01:43:49 -0400

    Added Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarCreateComponent } from './cars/car-create/car-create.component';
+import { CarListComponent } from './cars/car-list/car-list.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: CarListComponent },
+  { path: 'host', component: CarCreateComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
