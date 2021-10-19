52d04e70a2f120b8149469750b5a5ca96219703a
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-10-19 12:46:56 -0400

    Organized Backend code

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const cors = require('cors')
 app.use(cors())
+
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://safa:Wj1S0Prpt6kXsALV@cluster0.15g5i.mongodb.net/car-rentals?retryWrites=true&w=majority")
   .then(() => {
@@ -14,8 +16,9 @@ mongoose.connect("mongodb+srv://safa:Wj1S0Prpt6kXsALV@cluster0.15g5i.mongodb.net
     console.log("Connection failed")
   })
 
+const carsRoutes = require('./controllers/carController.js')
+app.use("/api/cars", carsRoutes);
 
-const Car = require('./models/car')
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -29,57 +32,4 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/cars", (req, res, next) => {
-  const car = new Car({
-    title: req.body.title,
-    description: req.body.description,
-    img: req.body.img
-  });
-  car.save().then(result => {
-    res.status(201).json({
-      message: 'Car added successfully',
-      carId: createdCar._id
-    });
-  });
-
-});
-app.put("/api/cars/:id", (req, res, next) => {
-  const car = new Car({
-    _id: req.body.id,
-    title: req.body.title,
-    description: req.body.description,
-    img: req.body.img
-  })
-  Car.updateOne({ _id: req.params.id }, car).then(updatedCar => {
-    req.status(200).json({ message: 'Car data Updated' })
-  })
-})
-app.get("/api/cars", (req, res, next) => {
-  Car.find()
-    .then(cars => {
-      console.log(cars)
-      res.status(200).json({
-        message: "Cars fetched successfully!",
-        cars: cars
-      });
-    });
-});
-
-app.get("/api/cars/:id", (req, res, next) => {
-  Car.findById(req.params.id).then(car => {
-    if (car) {
-      res.status(200).json(car)
-    } else {
-      res.status(404).json({ message: 'Car not found!' });
-    }
-  })
-})
-//Delete
-
-app.delete("/api/cars/:id", (req, res, next) => {
-  Car.deleteOne({ _id: req.params.id }).then(deletedCar => {
-    console.log(deletedCar)
-    res.status(200).json({ message: "Car Deleted!" })
-  })
-});
 module.exports = app;
---
+++ backend/controllers/carController.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Car = require('../models/car')
+
+router.post("", (req, res, next) => {
+    const car = new Car({
+        title: req.body.title,
+        description: req.body.description,
+        img: req.body.img
+    });
+    car.save().then(result => {
+        res.status(201).json({
+            message: 'Car added successfully',
+            carId: createdCar._id
+        });
+    });
+
+});
+router.put("/:id", (req, res, next) => {
+    const car = new Car({
+        _id: req.body.id,
+        title: req.body.title,
+        description: req.body.description,
+        img: req.body.img
+    })
+    Car.updateOne({ _id: req.params.id }, car).then(updatedCar => {
+        req.status(200).json({ message: 'Car data Updated' })
+    })
+})
+router.get("", (req, res, next) => {
+    Car.find()
+        .then(cars => {
+            console.log(cars)
+            res.status(200).json({
+                message: "Cars fetched successfully!",
+                cars: cars
+            });
+        });
+});
+
+router.get("/:id", (req, res, next) => {
+    Car.findById(req.params.id).then(car => {
+        if (car) {
+            res.status(200).json(car)
+        } else {
+            res.status(404).json({ message: 'Car not found!' });
+        }
+    })
+})
+//Delete
+
+router.delete("/:id", (req, res, next) => {
+    Car.deleteOne({ _id: req.params.id }).then(deletedCar => {
+        console.log(deletedCar)
+        res.status(200).json({ message: "Car Deleted!" })
+    })
+});
+
+module.exports = router
