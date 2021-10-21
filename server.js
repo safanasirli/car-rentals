dbd27560284969f4cb6d13ad51abae01fa9ce6b8
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-10-21 14:44:06 -0400

    Test

--- server.js
+++ server.js
@@ -1,57 +1,15 @@
 const app = require("./backend/app");
 const express = require('express');
-const debug = require("debug")("node-angular");
 const http = require("http");
 
-const normalizePort = val => {
-    var port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-};
-
-const onError = error => {
-    if (error.syscall !== "listen") {
-        throw error;
-    }
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-    switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
 app.use(express.static('./dist/car-rentals-frontend'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/car-rentals-frontend/'}),
 );
-const onListening = () => {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-    debug("Listening on " + bind);
-};
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = (process.env.PORT || "3000");
 app.set("port", port);
 
 const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
 server.listen(port);
