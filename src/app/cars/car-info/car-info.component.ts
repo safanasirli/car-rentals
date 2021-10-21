ad0e84bfede6dd14ab805b5445ecc30d7fa55353
Safa Nasirli <admin@admins-air.myfiosgateway.com>
2021-10-21 02:51:25 -0400

    Debugging on  Delete and Edit Buttons

--- src/app/cars/car-info/car-info.component.ts
+++ src/app/cars/car-info/car-info.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Car } from '../cars.model'
 import { CarsService } from '../cars.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'car-info',
@@ -9,17 +10,33 @@ import { CarsService } from '../cars.service';
     styleUrls: ['./car-info.component.css']
 })
 
-export class CarInfoComponent implements OnInit {
+export class CarInfoComponent implements OnInit, OnDestroy {
     car: Car
-    constructor(public carsService: CarsService, private route: ActivatedRoute,) {
+    cars: Car[] = []
+    private carsSub: Subscription
+    constructor(public carsService: CarsService, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
+
         const id = this.route.snapshot.paramMap.get('carId');
+
         this.carsService.getCar(id)
             .subscribe((carData) => {
                 this.car = { id: carData._id, title: carData.title, description: carData.description, img: carData.img, price: carData.price }
             })
+        this.carsService.getCars()
+        this.carsSub = this.carsService.getCarUpdateListener()
+            .subscribe((cars: Car[]) => {
+                this.cars = cars
+            })
+
+    }
+    onDelete(carId: string) {
+        this.carsService.deleteCar(carId)
+    }
+    ngOnDestroy() {
+        this.carsSub.unsubscribe()
     }
 
 }
